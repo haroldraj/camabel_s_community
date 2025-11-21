71eaf88f93b8a6408aa6ec178ad1d5dda9a91640
Harold Rajaonarison <rnarsonharold@gmail.com>
2025-11-21 12:42:07 +0100

    working on navigation bar

---
+++ lib/screens/spiritual_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SpiritualScreen extends StatefulWidget {
+  const SpiritualScreen({super.key});
+
+  @override
+  State<SpiritualScreen> createState() => _SpiritualScreenState();
+}
+
+class _SpiritualScreenState extends State<SpiritualScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
